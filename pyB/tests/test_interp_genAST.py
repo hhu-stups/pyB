2f742b6702bc3fdbf75f1c66438cc5b3e04cd220
John Witulski <john.witulski@uni-duesseldorf.de>
2011-10-04 11:35:09 +0000

    the tests from that file are copyed to the test_interp_*.py files.
    git-svn-id: https://cobra.cs.uni-duesseldorf.de/prob/branches/witulski@9134 7aec93f6-bc54-0410-ac70-7d7c9efa889a

--- pyB/tests/test_interp_genAST.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from ast_nodes import *
-from interp import inperpret, Environment
-from helpers import file_to_AST_str, string_to_file
-
-file_name = "input.txt"
-
-class TestGenAST():
-    pass
