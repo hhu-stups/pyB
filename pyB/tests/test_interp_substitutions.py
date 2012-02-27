93496fc6e61c7a1c45df9bda2eac9667fd23d0b0
John Witulski <john.witulski@uni-duesseldorf.de>
2012-02-27 15:50:02 +0000

    added substitution tests
    git-svn-id: https://cobra.cs.uni-duesseldorf.de/prob/branches/witulski@10216 7aec93f6-bc54-0410-ac70-7d7c9efa889a

---
+++ pyB/tests/test_interp_substitutions.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# also typchecking-tests
+from ast_nodes import *
+from interp import Environment
+from typing import _test_typeit, IntegerType, PowerSetType, SetType, CartType, EmptySetType, StringType, BoolType
+from interp import interpret, Environment
+from helpers import file_to_AST_str, string_to_file
+
+file_name = "input.txt"
+
+class TestInterpSubstitutions():
+    def test_genAST_sub_simple_asgn(self):
+        # Build AST
+        string = '''
+        MACHINE Test
+        VARIABLES xx
+        INVARIANT xx:NAT
+        INITIALISATION xx:=3
+        END'''
+        string_to_file(string, file_name)
+        ast_string = file_to_AST_str(file_name)
+        exec ast_string
+
+        # Test
+        env = Environment()
+        interpret(root, env) # init VARIABLES and eval INVARIANT
+        assert isinstance(root.children[1], AInvariantMachineClause)
+        assert interpret(root.children[1], env)
+        assert env.get_value("xx")==3
+        assert isinstance(env.get_type("xx"), IntegerType)
+
+
+
+    def test_genAST_sub_parallel_asgn(self):
+        # Build AST
+        string = '''
+        MACHINE Test
+        VARIABLES xx, yy
+        INVARIANT xx:NAT & yy:NAT
+        INITIALISATION xx:=1 || yy:= 2
+        END'''
+        string_to_file(string, file_name)
+        ast_string = file_to_AST_str(file_name)
+        exec ast_string
+
+        # Test
+        env = Environment()
+        interpret(root, env) # init VARIABLES and eval INVARIANT
+        assert isinstance(root.children[1], AInvariantMachineClause)
+        assert interpret(root.children[1], env)
+        assert env.get_value("xx")==1
+        assert env.get_value("yy")==2
+        assert isinstance(env.get_type("xx"), IntegerType)
+        assert isinstance(env.get_type("yy"), IntegerType)
+
+
+    def test_genAST_sub_multiple_asgn(self):
+        # Build AST
+        string = '''
+        MACHINE Test
+        VARIABLES xx, yy
+        INVARIANT xx:NAT & yy:NAT
+        INITIALISATION xx,yy:=1,2
+        END'''
+        string_to_file(string, file_name)
+        ast_string = file_to_AST_str(file_name)
+        exec ast_string
+
+        # Test
+        env = Environment()
+        interpret(root, env) # init VARIABLES and eval INVARIANT
+        assert isinstance(root.children[1], AInvariantMachineClause)
+        assert interpret(root.children[1], env)
+        assert env.get_value("xx")==1
+        assert env.get_value("yy")==2
+        assert isinstance(env.get_type("xx"), IntegerType)
+        assert isinstance(env.get_type("yy"), IntegerType)
