ea2ca5eee7da06d20f7e8d0848790d2cbf1fa368
John Witulski <john.witulski@uni-duesseldorf.de>
2012-09-21 14:39:08 +0000

    git-svn-id: https://cobra.cs.uni-duesseldorf.de/prob/branches/witulski@11887 7aec93f6-bc54-0410-ac70-7d7c9efa889a

---
+++ pyB/bexceptions.py
@@ -0,0 +1,3 @@
+class ValueNotInDomainException:   
+    def __init__(self, value):
+        self.value = value
--- pyB/config.py
+++ pyB/config.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 min_int = -1
-max_int = 5
+max_int = 10000
 max_op_solutions = 4
 enable_assertions = False
 deferred_set_elements_num = 3
