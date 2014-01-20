af49e1e407553772c8625cb403674f8e93507bdd
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2014-01-20 15:59:22 +0100

    test commit

--- pyB/tests/util.py
+++ pyB/tests/util.py
@@ -2,8 +2,7 @@
 from interp import set_up_constants, exec_initialisation
 
 # The data from the solution-files has already been read at the mch creation time.
-# side-effect of this method may be a state change.
-# arbitrary
+# side-effect of this method: may be a state change.
 def arbitrary_init_machine(root, env, mch, solution_file_read=False):    
     bstates = set_up_constants(root, env, mch, solution_file_read)
     if len(bstates)>0:
