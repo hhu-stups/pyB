b88ecf66938fdbe8b7f8c30a767bf577bc712a50
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2014-08-13 14:29:50 +0200

    added more symbolic sequence tests (none pass)

--- pyB/tests/test_symbolic_sequences.py
+++ pyB/tests/test_symbolic_sequences.py
@@ -11,6 +11,8 @@ from symbolic_sets import *
 
 file_name = "input.txt"
 
+# TODO: front, tail, conc, last, conc
+# maybe this is only importent in theory...
 class TestSymbolicSequences():
     import pytest
     @pytest.mark.xfail 
@@ -25,6 +27,7 @@ class TestSymbolicSequences():
         assert 1==2 # Timeout exception missing
         assert interpret(root, env)
 
+
     import pytest
     @pytest.mark.xfail         
     def test_symbolic_sequences_first2(self):
@@ -37,3 +40,128 @@ class TestSymbolicSequences():
         env = Environment()
         assert 1==2 # Timeout exception missing
         assert interpret(root, env)
+
+
+    import pytest
+    @pytest.mark.xfail         
+    def test_symbolic_sequences_first2(self):
+        # Build AST
+        # f(1)=1
+        string_to_file("#PREDICATE first(%x.(x:INTEGER|x))=1", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert 1==2 # Timeout exception missing
+        assert interpret(root, env)
+        
+        
+    import pytest
+    @pytest.mark.xfail 
+    def test_symbolic_sequences_size(self):
+        # Build AST
+        string_to_file("#PREDICATE size(%x.(x:NATURAL & x<2**32|x))=2**32", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)    
+        
+        
+    # TODO: undefined size. Throw exception
+    import pytest
+    @pytest.mark.xfail     
+    def test_symbolic_sequences_size2(self):
+        # Build AST
+        string_to_file("#EXPRESSION size(%x.(x:NATURAL|x))", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)          
+ 
+ 
+    # seq. is prob calc result
+    import pytest
+    @pytest.mark.xfail 
+    def test_symbolic_sequences_rev(self):
+        # Build AST
+        string_to_file("#PREDICATE rev(%x.(x:NATURAL & x<5|x))={(2,4),(3,3),(4,2),(5,1),(6,0)}", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)              
+        
+
+    #missing or wrong impl. for __getitem__
+    import pytest
+    @pytest.mark.xfail         
+    def test_symbolic_sequences_conc(self):
+        # Build AST
+        string_to_file("#EXPRESSION (%x.(x:NAT1|x+1))^(%x.(x:NAT1|x*x))", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env) 
+ 
+        
+    #missing  or wrong impl. for __getitem__
+    import pytest
+    @pytest.mark.xfail    
+    def test_symbolic_sequences_append(self):
+        # Build AST
+        # {(1,2)...(127,128),(128,42)}
+        string_to_file("#EXPRESSION (%x.(x:NAT1|x+1))<-42", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)        
+
+
+    import pytest
+    @pytest.mark.xfail 
+    def test_symbolic_sequences_prepend(self):
+        # Build AST
+        # {(1,32),(2,2)...(128,128)}
+        string_to_file("#EXPRESSION 42->(%x.(x:NAT1|x+1))  ", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)        
+ 
+ 
+    import pytest
+    @pytest.mark.xfail   
+    def test_symbolic_sequences_take(self):
+        # Build AST
+        string_to_file("#PREDICATE (%x.(x:NAT1|x+1))/|\\3=[2,3,4]", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)    
+    
+    import pytest
+    @pytest.mark.xfail 
+    def test_symbolic_sequences_drop(self):
+        # Build AST
+        string_to_file("#PREDICATE (%x.(x:NAT1|x+1))\\|/MAXINT-3=[MAXINT-1,MAXINT,MAXINT+1]", file_name)
+        ast_string = file_to_AST_str(file_name)
+        root = str_ast_to_python_ast(ast_string)
+        
+        # Test 
+        env = Environment()
+        assert interpret(root, env)    
+    
