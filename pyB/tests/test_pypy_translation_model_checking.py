2f777bbe97b4f225f3ef9af9241b6cd9b74e856b
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2015-09-23 18:01:11 +0200

    added new test-file documenting every working PyB-c model checking example

---
+++ pyB/tests/test_pypy_translation_model_checking.py
@@ -0,0 +1,68 @@
+# assumes pypy checkout at PYPYDIR
+from config import USE_RPYTHON_CODE
+
+file_name = "input.txt"
+PYPY_DIR  = "/Users/johnwitulski/witulski/git/pyB/pypy/" # change this line to your checkout
+
+
+# generates c-PyB and python-PyB executables 
+def translate():  
+    import os
+    from subprocess import Popen, PIPE
+    if os.name=='nt':
+        # Add pypy to your path if this line crashs
+        Popen("python ../pypy/rpython/translator/goal/translate.py --batch pyB_RPython.py", shell=True, stdout=PIPE).stdout.read()
+    else:
+        pwd = Popen("pwd", shell=True, stdout=PIPE).stdout.read()
+        assert pwd[-4:]=='pyB\n'
+        Popen("PYTHONPATH="+PYPY_DIR+":. python ../pypy/rpython/translator/goal/translate.py --batch pyB_RPython.py", shell=True, stdout=PIPE).stdout.read()
+
+
+# Run main_code with CPython (except RPython Flags) and Translated C Version
+def execute_file(cl_argument=""):
+    from subprocess import Popen, PIPE
+    c_result = Popen("./"+"pyB_RPython-c "+cl_argument, shell=True, stdout=PIPE).stdout.read()
+    return c_result
+  
+    
+def clean_up():
+    import os
+    #os.remove("pyB_RPython-c")
+
+import pytest, config
+@pytest.mark.skipif(config.USE_RPYTHON_CODE==False, reason="translation to c not possible using built-in frozenset type")     
+class TestPyPyTranslationModelChecking():
+    def test_pypy_mc_lift(self):
+        translate() 
+        
+        c_result = execute_file(" -mc examples/Lift2.mch")       
+        expected ="""\033[1m\033[91mWARNING\033[00m: model checking still experimental
+        checked 100 states. No invariant violation found.
+        """
+        assert c_result.replace(" ", "").replace("\t", "").split('\n') ==  expected.replace(" ", "").replace("\t", "").split('\n') 
+        
+        c_result = execute_file(" -mc examples/Lift.mch")
+        expected ="""\033[1m\033[91mWARNING\033[00m: model checking still experimental
+        \033[1m\033[91mWARNING\033[00mInvariant violation
+
+        FALSE Predicates:
+        FALSE = (floor:0 .. 99)
+        dec
+        dec
+        dec
+        dec
+        dec
+        init
+
+        WARNING: invariant violation found after checking 101 states
+        """
+        assert c_result.replace(" ", "").replace("\t", "").split('\n') ==  expected.replace(" ", "").replace("\t", "").split('\n') 
+        
+        c_result = execute_file(" -mc examples/WhileLoop.mch")
+        expected ="""\033[1m\033[91mWARNING\033[00m: model checking still experimental
+        \033[1m\033[91mWARNING\033[00m: WHILE inside abstract MACHINE!
+        checked 2 states. No invariant violation found.
+        """
+        assert c_result.replace(" ", "").replace("\t", "").split('\n') ==  expected.replace(" ", "").replace("\t", "").split('\n') 
+        
+        clean_up()
