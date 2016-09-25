f0f6592971800f057ec6a4d2af54ca3fb1a36bee
JonnyWalker <John.Witulski@uni-duesseldorf.de>
2016-09-26 01:03:43 +0200

    fixed run benchmarks

--- pyB/run_benchmarks.sh
+++ pyB/run_benchmarks.sh
@@ -1,13 +1,20 @@
-MCH = Lift2.mch         # Table 6.1
-MCH = SetUnion.mch      # Table 6.2
-MCH = WhileLoop.mch     # Table 6.3
-MCH = WhileLoop2.mch    # Table 6.4
-MCH = SigmaLoop.mch     # Table 6.5
-MCH = SigmaLoop2.mch     # Table 6.5
+doStuff()
+{
 echo "+++++++"
 echo $MCH
 echo "+++++++"
-time ./pyB_RPython-c -mc examples/rpython_performance/Lift2.mch 
-time PYTHONPATH=.:../pypy python pyB_RPython.py -mc examples/rpython_performance/Lift2.mch
-time python pyB.py -mc examples/rpython_performance/Lift2.mch
-time ../ProB/probcli -mc 2147483648 -p MAXINT 2147483648 -p MININT -2147483648 -p TIME_OUT 2147483648 examples/rpython_performance/Lift2.mch
+time ./pyB_RPython-c -mc examples/rpython_performance/$MCH 
+time PYTHONPATH=.:../pypy python pyB_RPython.py -mc examples/rpython_performance/$MCH
+time python pyB.py -mc examples/rpython_performance/$MCH
+time ../ProB/probcli -mc 2147483648 -p MAXINT 2147483648 -p MININT -2147483648 -p TIME_OUT 1000000 examples/rpython_performance/$MCH
+}
+
+MCH=Lift2.mch         # Table 6.1
+doStuff
+MCH=SetUnion.mch      # Table 6.2
+doStuff
+MCH=WhileLoop.mch     # Table 6.3
+doStuff
+MCH=WhileLoop2.mch    # Table 6.4
+MCH=SigmaLoop.mch     # Table 6.5
+MCH=SigmaLoop2.mch    # Table 6.5
